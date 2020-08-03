ce8595dc855e0b23be5ef261c1b51c8406ac20bf
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2020-08-03 18:41:55 +0200

    Changed some text for logon "user"

--- src/components/header.js
+++ src/components/header.js
@@ -10,10 +10,12 @@ function Header() {
   return (
     <div className="Header">
       Header component, here will be some text and logo.
-      <button onClick={() => dispatch(isLoged())}>
+      <div style={{ float: "right", marginLeft: "1em" }}>
+        {loged ? <div>Welcome Mr.X</div> : <div>Not loged in</div>}
+      </div>
+      <button style={{ float: "right" }} onClick={() => dispatch(isLoged())}>
         {loged ? <span>Logout</span> : <span>Login</span>}
       </button>
-      <div>{loged ? <div>Yes</div> : <div>No</div>}</div>
     </div>
   );
 }
